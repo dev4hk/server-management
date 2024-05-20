34e7f0e101a0b9e147e4442475bc2cc1570d14bf
Hoon <hoon0661@gmail.com>
2024-05-20 15:36:29 -0400

    added form - to add server to the list

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -27,6 +27,8 @@ export class AppComponent implements OnInit {
   private filterSubject = new BehaviorSubject<string>('');
   private dataSubject = new BehaviorSubject<CustomResponse>(null);
   filterStatus$ = this.filterSubject.asObservable();
+  private isLoading = new BehaviorSubject<boolean>(false);
+  isLoading$ = this.isLoading.asObservable();
 
   constructor(private serverService: ServerService) {}
 
@@ -97,6 +99,7 @@ export class AppComponent implements OnInit {
   }
 
   saveServer(serverForm: NgForm): void {
+    this.isLoading.next(true);
     this.appState$ = this.serverService.save$(serverForm.value as Server).pipe(
       map((response) => {
         this.dataSubject.next({
@@ -109,6 +112,7 @@ export class AppComponent implements OnInit {
           },
         });
         document.getElementById('closeModal').click();
+        this.isLoading.next(false);
         serverForm.resetForm({ status: this.Status.SERVER_DOWN });
         return {
           dataState: DataState.LOADED_STATE,
@@ -120,6 +124,7 @@ export class AppComponent implements OnInit {
         appData: this.dataSubject.value,
       }),
       catchError((error: string) => {
+        this.isLoading.next(false);
         return of({ dataState: DataState.ERROR_STATE, error });
       })
     );
